7eb55d1a84af6513bcd5744cd24d15a254e06a50
Nicolas Arias <30639530+NicolasArias@users.noreply.github.com>
2022-02-26 10:06:17 -0500

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef LOCAL
+#include "../debug.h"
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 42
+#endif
+
+
+
+
+
+int main(){
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	return 0;
+}
