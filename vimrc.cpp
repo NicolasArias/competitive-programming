cc0693a0b5889185010f02f410720e2b92b4f4d8
Nicolas Arias <30639530+NicolasArias@users.noreply.github.com>
2023-12-13 21:19:46 -0500

    Update vimrc.cpp

--- vimrc.cpp
+++ vimrc.cpp
@@ -1,16 +1,19 @@
 syntax on
+set termguicolors
+set background=dark
+colorscheme aurora
+hi Normal guibg=NONE ctermbg=NONE
 set number
 set noerrorbells
-set noerrorbells visualbell t_vb=
-autocmd GUIEnter * set visuabell t_vb =
 set tabstop=2 softtabstop=2
-set shiftwidth=2
+se shiftwidth=2
 set autoindent
 set smartindent
 set nowrap
+"highlight CursorLine cterm=NONE ctermbg=NONE ctermfg=NONE guibg=NONE guifg=NONE
+set cursorline
 set clipboard=unnamedplus
 "intall vim-gtk o vim-gnome
-"for windows
 let s:clip = '/mnt/c/Windows/System32/clip.exe'  " default location
 if executable(s:clip)
 	augroup WSLYank
@@ -18,6 +21,17 @@ if executable(s:clip)
 		autocmd TextYankPost * call system('echo '.shellescape(join(v:event.regcontents, "\<CR>")).' | '.s:clip)
 		augroup END
 endif
+call plug#begin('~/.vim/plugged')
+" The default plugin directory will be as follows:
+"   - Vim (Linux/macOS): '~/.vim/plugged'
+"   - Vim (Windows): '~/vimfiles/plugged'
+"   - Neovim (Linux/macOS/Windows): stdpath('data') . '/plugged'
+" You can specify a custom plugin directory by passing it as the argument
+"   - e.g. `call plug#begin('~/.vim/plugged')`
+"   - Avoid using standard Vim directory names like 'plugin'
+Plug 'rafalbromirski/vim-aurora'
+Plug 'octol/vim-cpp-enhanced-highlight'
+call plug#end()
 nnoremap<F1> :!./%:r.out < in1 <CR>
 nnoremap<F2> :!./%:r.out < in2 <CR>
 nnoremap<F3> :!./%:r.out < in3 <CR>
@@ -25,4 +39,5 @@ nnoremap<F4> :!./%:r.out < in4 <CR>
 nnoremap<F5> :w <bar> !g++ -DLOCAL -o %:r.out % -std=c++17<Enter>
 nnoremap<F6> :%y+<CR>
 nnoremap<F9> :!./%:r.out<Enter>
+
 
